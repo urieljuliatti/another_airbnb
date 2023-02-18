25e92c08fb635409433b408d2ddb4e370b270a4e
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-18 10:44:39 -0300

    Adicionando modelo de has_one

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_133231) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_133649) do
+  create_table "bookings", force: :cascade do |t|
+    t.time "checkin"
+    t.time "checkout"
+    t.integer "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_bookings_on_room_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.boolean "checked"
     t.float "price"
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_133231) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "rooms"
 end
