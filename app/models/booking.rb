f51240150a41f2bc1702e4348d93690c1b5e96a5
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-18 11:18:43 -0300

    Adicionando o belongs_to booking ao User e has_one ao Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :room
+  has_one :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_133649) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_141157) do
   create_table "bookings", force: :cascade do |t|
     t.time "checkin"
     t.time "checkout"
@@ -37,9 +37,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_133649) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
+    t.index ["booking_id"], name: "index_users_on_booking_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "rooms"
+  add_foreign_key "users", "bookings"
 end
