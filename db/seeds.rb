63e8ae3cae775ed10bee5c9ea963fb16b5bb20ad
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-19 15:58:38 -0300

    Adicionando um segundo usuário no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 User.create(email: 'user@me.com', password: '123456')
+User.create(email: 'user2@me.com', password: '123456')
 Room.create(title: 'Room #1', price: 15, body: 'Room #1 Description')
 Room.create(title: 'Room #2', price: 30, body: 'Room #2 Description')
