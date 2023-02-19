7e7518c250cc2a7ffeee4ae275ea99d1f4deaad5
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-19 11:59:58 -0300

    Adicionando Timezone de Sao Paulo

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,7 @@ class BookingsController < ApplicationController
     if @user.booking
       flash[:error] = 'Você não pode alugar mais de um quarto.'
     else
-      @booking = Booking.new(room: @room, user_id: @user.id, checkin: Time.now)
+      @booking = Booking.new(room: @room, user_id: @user.id, checkin: Time.zone.now)
       @booking.save
       @user.booking_id = @booking.id
       @user.save
@@ -15,7 +15,7 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking = Booking.find(params[:id])
-    @booking.checkout = Time.now
+    @booking.checkout = Time.zone.now
     @booking.destroy
     redirect_to rooms_path, notice: 'Checkout feito com sucesso.'
   end
