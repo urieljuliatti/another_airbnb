aeead18b3931221cd0d50911ea209a885b0d977c
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-20 10:19:55 -0300

    Adicionando autorização para Admin

--- app/controllers/admin/booking_histories_controller.rb
+++ app/controllers/admin/booking_histories_controller.rb
@@ -1,8 +1,13 @@
 class Admin::BookingHistoriesController < ApplicationController
 
   before_action :authenticate_user! 
-  
+  before_action :authorized_user
   def index
     @booking_histories = BookingHistory.all
+  end
+
+  private
+  def authorized_user
+    redirect_to rooms_path, notice: 'Você não possui autorização.' unless current_user.admin?
   end
 end
