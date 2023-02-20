257186f2f1763faed7a0dd5ec0ecb729c8f8cf9d
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-20 14:55:02 -0300

    Quick fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import * as bootstrap from "bootstrap";
