0abc1e85fad0aba261e87581d511bf48187bc4d7
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-21 10:52:35 -0300

    Adicionando check in para quem não alugou uma sala

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -30,10 +30,24 @@ RSpec.describe BookingsController, type: :controller do
   end
 
   describe 'POST checkin sem booking' do
-    subject { post :create, params: { room_id: room.id, booking: { user_id: customer.id } } }
+    
+    let(:another_room) { create(:room) }
+    let(:another_booking) { create(:booking) }
+    before do
+      another_booking.user = create(:user)
+      customer.booking = nil
+      customer.save
+      another_booking.save
+    end
+
+    subject { post :create, params: { room_id: another_room.id, booking: { user_id: customer.id } } }
 
     it 'redirects_to rooms_path' do
       expect(subject).to redirect_to(rooms_url)
+    end
+    it 'can rent a room' do
+      subject
+      expect(flash[:notice]).to match(/Checkin feito com sucesso.*/)
     end
   end
 
