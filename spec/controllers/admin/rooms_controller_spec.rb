528dfdde6177df0524b2fe2f1ec48b53b1a8ef23
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2023-02-21 13:46:39 -0300

    Adicionando teste para Admin::RoomsController

---
+++ spec/controllers/admin/rooms_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'support/devise'
+
+RSpec.describe Admin::RoomsController, type: :controller do
+
+  login_admin
+
+  let(:room_attributes) { attributes_for :room }
+
+  describe 'POST create' do
+    subject { post :create, params: { room: room_attributes } }
+
+    it 'redirects_to rooms_path' do
+      expect(subject).to redirect_to(rooms_url)
+    end
+  end
+end
